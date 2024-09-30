0e1083920ec0f1b2863570d136473fdabf10e250
Majid Ali <majid@teresol.com>
2024-10-01 03:18:02 +0500

    version file added

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 func main() {
-	PrintVersion() // Print version
+	PrintVersion() // Print version information
 	makeMaps()     // make sure all maps are initialized
 	makeStructs()  // make sure all structs are initialized
 	makeArray()    // make sure all arrays are initialized
