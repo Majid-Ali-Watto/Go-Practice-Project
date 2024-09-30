31d57a78477ac83b8ee869ec8136913b1ebadd60
Majid-Ali-Watto <Majid-Ali-Watto@users.noreply.github.com>
2024-09-30 22:18:27 +0000

    Bump version to 0.0.2

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ import (
 
 // Version represents the current version of the application
 
-var Version = "0.0.1"
+var Version = "0.0.2"
 
 // PrintVersion prints the current version
 func PrintVersion() {
