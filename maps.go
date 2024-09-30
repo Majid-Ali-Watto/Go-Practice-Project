b57f4ff2ffa27a0d91b0d79def074c5e351ef9d0
Majid Ali <majid@teresol.com>
2024-09-30 22:27:21 +0500

    maps added

--- maps.go
+++ maps.go
@@ -10,6 +10,7 @@ func singleMap(name string) map[string]string {
 }
 func makeMaps() {
 	maps := []map[string]string{}
+	// maps := make(map[string]string,5)
 	maps = append(maps, singleMap("Majid"))
 	maps = append(maps, singleMap("Ali"))
 	fmt.Println(maps)
