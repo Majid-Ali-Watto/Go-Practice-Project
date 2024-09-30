ff714e05643b2ff326da6fe653227fcdd13b474d
Majid Ali <majid@teresol.com>
2024-10-01 02:01:26 +0500

    version file added

---
+++ version.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Version represents the current version of the application
+var Version = "0.0.1" // Initial version
+
+// PrintVersion prints the current version
+func PrintVersion() {
+	fmt.Println("Current Version:", Version)
+}
