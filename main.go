e6e07771c596caa759b2d5d23835a1c61a34fdfa
Majid Ali <majid@teresol.com>
2024-10-01 01:54:38 +0500

    comment added

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 func main() {
-	makeMaps()
-	makeStructs()
-	makeArray()
+	makeMaps()    // make sure all maps are initialized
+	makeStructs() // make sure all structs are initialized
+	makeArray()   // make sure all arrays are initialized
 }
